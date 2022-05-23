af7540348d981908e3ba281892dc10628f4e2dc0
Pablo <delatorre.pablodaniel@gmail.com>
2022-05-23 01:38:39 -0300

    proyecto encaminado

---
+++ servidor.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const { Server : ioServer } = require('socket.io')
+const http = require('http')
+const app = express()
+const multer = require('multer')
+const routesProductos = require("./routes/routes-productos.js")
+
+
+const httpServer = http.createServer(app)
+const io = new ioServer(httpServer)
+
+app.use(multer({
+    dest:__dirname+"/public/files",
+
+}).single("photo"))
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(express.static(__dirname+"/public"))
+app.use('/productos', routesProductos)
+app.set('views','./views')
+app.set('view engine','ejs')
+
+app.get('/',(req,res) => {
+    res.render('index', {
+        title:"Agregue un producto"
+    })
+})
+
+io.on('connection',()=>{
+    console.log("websocket funcionando")
+})
+
+const PORT = 8080
+const server = httpServer.listen(PORT, () => {
+    console.log(`servidor escuchando en el puerto ${PORT}`)
+})
